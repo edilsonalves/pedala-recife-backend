640b0ba93b871a93f3f159bd462fe93eb9033deb
Edilson Alves <email@edilsonalves.com>
2020-07-29 09:27:37 -0300

    Add presentation page

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import roadRouter from './routes/road.router';
 
 const app = express();
@@ -7,5 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(roadRouter);
+app.use(express.static(path.join(__dirname, '../', 'public')));
 
 export default app;
